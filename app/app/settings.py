1704facab9c1c6af1ce3b46bbf2029f5c973966a
lorczyslav <lorczyslav@gmail.com>
2019-11-21 01:15:07 +0100

    added directory structure for advert app, adjusted global settings and urls

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core',
     'user',
-    'message'
+    'message',
+    'advert'
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/message/', include('message.urls')),
+    path('api/advert/', include('advert.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
